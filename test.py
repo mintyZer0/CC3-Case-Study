2769f6692aa7bc0ac574848983433920b5c3a43e
Thristan Dave <thristandave12@gmail.com>
2025-03-17 13:37:22 +0800

    Center justify welcome message and thank you message; improve formatting in welcome_page function

--- test.py
+++ test.py
@@ -22,64 +22,54 @@ def separator():
     
 def welcome_page():
     separator()
-    print("         Welcome to AE Merchandise Store")
+    print(f"{"Welcome to AE Merchandise Store":^50}") #center justify
     separator()
-    print(f"Transaction ID: {transaction_id():>35}")
-    print(f"Date & Time: {date_time():>38}\n")
+    print(f"Transaction ID: {transaction_id():>35}") #right justify
+    print(f"Date & Time: {date_time():>38}\n") #right justify
     separator()
 
 def purchase_type():
-    
     choice = input("Enter purchase type ('bulk' or 'retail'): ")
     return choice
 
 def number_of_items():
-    
     n = int(input("Enter number of items: "))
     return n
     
 def main():
-    
     is_running = True
-    
     welcome_page()
     
     while is_running:
-        
         n = number_of_items()
         
         for i in range(1, (n + 1)):
-            pass
-            # you can list items here
+            
             try:
                 choice = purchase_type()
-                
                 if choice.lower() == 'bulk':
                     pass
                 elif choice.lower() == 'retail':
                     pass
                 else:
                     raise ValueError("Invalid purchase type")
-
             except ValueError as ve:
                 print(f"Error: {ve}")
                 
         while True:
                     buy_again = input("Would you like to add something ('y' or 'n')? ")
-                    
                     try:
                         if buy_again.lower() == 'y':
                             print()
                             break
-                        
                         elif buy_again.lower() == 'n':
-                            print("Thank you for your purchase. Goodbye!")
+                            separator()
+                            print(f"{"Thank you for your purchase. Goodbye!":^50}")
+                            separator()
                             is_running = False
-                            break
-                        
+                            break                        
                         else:
                             raise ValueError("Invalid choice")
-                        
                     except ValueError as ve:
                         print(f"Error: {ve}")
 
