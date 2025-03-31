343a4b073b6f401e9fd9ed2be64cf6bdf17d77b9
minty <eithanmathewmalonzo@gmail.com>
2025-03-31 14:23:59 +0800

    Added a print_cart() function which prints the current items that are in your car - changed the way transaction id is called so that it only gets called   and we retain the same transaction id per session

--- test.py
+++ test.py
@@ -1,9 +1,7 @@
 import random
-import sys
 from os import system
-import sys
 from datetime import datetime
-
+ 
 inventory_dict = {}
 purchased_items = []
 
@@ -25,12 +23,12 @@ def transaction_id():
 def separator():
     print("+" + "-"*100 + "+")
 
-def welcome_page():
+def welcome_page(transaction_id: str):
     #Prints a welcome page with transaction id and current date
     separator()
     print(f"{"Welcome to AE Merchandise Store":^100}") #center justify
     separator()
-    print(f"Transaction ID: {transaction_id():>85}") #right justify
+    print(f"Transaction ID: {transaction_id:>85}") #right justify
     print(f"Date & Time: {date_time():>88}") #right justify
     separator()
 
@@ -69,7 +67,7 @@ def get_item_input(category):
        quantity = int(quantity)
        for i, (key, value) in enumerate(inventory_dict[category].items()):
            if item == i + 1:
-               return key, quantity, quantity * int(value)
+               return key, quantity, quantity * int(value), value
 
 def print_items(category):
     #Prints items listed inside category
@@ -127,20 +125,38 @@ def generate_receipt():
     print(f"{"Receipt":^100}")
     separator()
     total_amount = 0
-    for item, quantity, cost in purchased_items:
-        print(f"{item:<50} x{quantity:<5} ₱{cost:>10}")
+    for item in purchased_items:
+        item_name, quantity, cost, *_ = item
+        print(f"{item_name:<50} x{quantity:<5} ₱{cost:>10}")
         total_amount += cost
     separator()
-    print(f"{'Total Amount:':<55} ₱{total_amount:>10}")
+    print(f"{'Total Amount:':<60} ₱{total_amount:^40}")
     separator()
+
+def print_cart():
+    max_length = 24
+    separator() 
+    print(f"{'Cart':^100}")
+    separator()
+    print(f"{'Qty':^25}|{'Item':^25}|{'Unit Price':^25}|{'Total Cost':^25}")
+    separator()
+    for item in purchased_items:
+        item_name = f"{item[0][0:max_length-4]}..." if len(item[0]) > max_length else item[0] 
+        price_per_unit = "₱" + str(item[3]) 
+        total_cost = "₱" + str(item[2])
+
+        print(f"{item[1]:^25}|{item_name:^25}|{price_per_unit:^25}|{total_cost:^25}")
 
 def main():
     is_running = True
+    transaction_id_instance = transaction_id()
     process_items()
-    welcome_page()
-    
     while is_running:
         while True:
+            #system('clear')
+            welcome_page(transaction_id_instance)
+            if purchased_items:
+                print_cart()
             print_menu()
             menu_input = get_menu_input()
             
@@ -156,22 +172,25 @@ def main():
                     choice = purchase_type()
                     category = menu_input
                     if choice.lower() == 'bulk':
-                        print_items(menu_input)
-                        item, quantity, total_cost = get_item_input(category)
+                        print_items(category)
+                        item, quantity, total_cost, price_per_unit = get_item_input(category)
                         if quantity < 20:
                             print("Quantity is less than 20, no discount applied.")
                             print(f"Total cost: ₱{total_cost}")
-                            purchased_items.append((item, quantity, total_cost))
+                            discounted = False
+                            purchased_items.append((item, quantity, total_cost, price_per_unit, discounted))
                         else:
                             discounted_price = apply_discount(total_cost)
                             print(f"Total cost: ₱{total_cost}")
                             print(f"Discounted price: ₱{discounted_price}")
-                            purchased_items.append((item, quantity, discounted_price))
+                            discounted = True
+                            purchased_items.append((item, quantity, discounted_price, price_per_unit, discounted))
                     elif choice.lower() == 'retail':
                         print_items(menu_input)
-                        item, quantity, total_cost = get_item_input(category)
+                        item, quantity, total_cost, price_per_unit = get_item_input(category)
                         print(f"Total cost: ₱{total_cost}")
-                        purchased_items.append((item, quantity, total_cost))
+                        discounted = False
+                        purchased_items.append((item, quantity, total_cost, price_per_unit, discounted))
                     else:
                         raise ValueError("Invalid purchase type")
                 except ValueError as ve:
@@ -193,7 +212,6 @@ def main():
                     raise ValueError("Invalid choice")
             except ValueError as ve:
                 print(f"Error: {ve}")
-
 main()
 # process_items()
 # get_menu_input()
